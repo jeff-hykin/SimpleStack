4d400278fe2cebaa4cfa2d05bead712225bd2a86
JeffHykin <jeff.hykin@gmail.com>
2018-02-28 00:50:13 -0600

    _

--- server/boilerplate/static/Home.page.js
+++ server/boilerplate/static/Home.page.js
@@ -1,13 +1,13 @@
 LoadChunk = async function(container) {
+// Create the title
 hello_element = New("p")
 hello_element.style = "width:100%;font-size:30pt;"
-hello_element.className = "center-align"
 hello_element.innerHTML = "Hello World!"
+Center(hello_element)
 container.appendChild(hello_element)
 
+// Add the hello.chunk 
 other_area = New("div")
 container.appendChild(other_area)
-
-
-blah = add({chunk:"hello", to:other_area})
+add({chunk:"hello", to:other_area})
 }
